90274736fce2fb7eebc8d8e58f17544515bc98f0
Andrysitraka <andrewsitraka@gmail.com>
2018-03-06 10:50:28 +0300

    ort sava 2018

---
+++ application/views/front/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Growing an agriculture Category Bootstrap Responsive website Template | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Growing Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="<?php echo base_url();?>/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="<?php echo base_url();?>/css/style.css" type="text/css" rel="stylesheet" media="all">   
+<link rel="stylesheet" href="<?php echo base_url();?>css/flexslider.css" type="text/css" media="all" property="" />   
+<!-- //Custom Theme files -->
+<!-- font-awesome icons -->
+<link href="<?php echo base_url();?>css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome icons -->
+<!-- js -->
+<script src="<?php echo base_url();?>/src/js/jquery-2.2.3.min.js"></script> 
+<!-- //js -->
+<!-- smooth scrolling -->
+<!-- web-fonts --> 
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<!-- //web-fonts --> 
+</head>
+<body>
+<!-- banner -->
+	<div class="banner">  
+		<div class="header agileinfo-header"><!-- header -->
+			<nav class="navbar navbar-default">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<h1><a href="<?php echo site_url();?>/ortsava/index">Growing</a></h1>
+					</div> 
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-left"> 
+							<li><a href="index.html" class="w3ls-hover active">Home</a></li>
+							<li><a href="about.html" class="btn w3ls-hover">About</a></li>   
+							<li><a href="gallery.html" class="btn w3ls-hover">Gallery</a></li>
+							<li><a href="#" class="dropdown-toggle btn w3ls-hover" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="icons.html">Web Icons</a></li>
+									<li><a href="codes.html">Short Codes</a></li>     
+								</ul>
+							</li>  
+							<li><a href="contact.html" class="btn w3ls-hover">Contact</a></li>
+						</ul>	   
+						<div class="social-icon">
+							<a href="#" class="social-button facebook"><i class="fa fa-facebook"></i></a> 
+							<a href="#" class="social-button twitter"><i class="fa fa-twitter"></i></a> 
+							<a href="#" class="social-button google"><i class="fa fa-google-plus"></i></a>  
+						</div> 
+						<div class="clearfix"> </div>
+					</div><!-- //navbar-collapse --> 
+				</div><!-- //container-fluid -->
+			</nav>
+		</div><!-- //header -->	
